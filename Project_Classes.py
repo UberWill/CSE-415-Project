69730f4b79be39b25f8e17a24ec5a25bbe2e134e
UberWilly <wbrugato2@gmail.com>
2015-05-21 20:37:13 -0700

    Project Classes
    Project Classes and icon files

---
+++ Project_Classes.py
@@ -0,0 +1,9 @@
+#the class to store ingredients
+class IngredientNode:
+
+    def __init__(self, name, calories, protein, carbs, fat):
+        self.name = name
+        self.calories = calories
+        self.protein = protein
+        self.carbs = carbs
+        self.fat = fat
